452ff436cfb46f9294b42ac098582be4af66c697
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-27 09:34:49 +0000

    entities file now creates a mario object and draws at the same time. function moved to layers.

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,18 @@
 import Compositor from './Compositor.js';
-import Entity from './Entity.js';
 import { loadLevel } from './loaders.js';
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
-import { createBackgroundLayer } from './layers.js';
+import {createMario} from './entities.js';
+import { loadBackgroundSprites } from './sprites.js'
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(entity) {
-    return function drawSpriteLayer(context) {
-        entity.draw(context);
-    }
-}
-
 Promise.all([
-    loadMarioSprite(),
+    createMario(),
     loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
-    .then(([marioSprite, backgroundSprites, level]) => {
+    .then(([mario, backgroundSprites, level]) => {
         const comp = new Compositor();
 
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
@@ -26,18 +20,7 @@ Promise.all([
 
         const gravity = 0.5;
 
-        const mario = new Entity();
-        mario.pos.set(64, 180);
-        mario.vel.set(2, -10);
 
-        mario.draw = function drawMario(context){
-            marioSprite.draw('idle', context, this.pos.x, this.pos.y);
-        }
-
-        mario.update = function updateMario() {
-            this.pos.x += this.vel.x;
-            this.pos.y += this.vel.y;
-        }
 
         const spriteLayer = createSpriteLayer(mario);
         comp.layers.push(spriteLayer);
