95e2d9e4015b0e68c2cb97cce9a931d1dfadd8a1
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-27 09:47:07 +0000

    delta time wip

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 import Compositor from './Compositor.js';
 import { loadLevel } from './loaders.js';
-import {createMario} from './entities.js';
+import { createMario } from './entities.js';
 import { loadBackgroundSprites } from './sprites.js'
 import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
@@ -16,21 +16,29 @@ Promise.all([
         const comp = new Compositor();
 
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-        comp.layers.push(backgroundLayer);
+        // comp.layers.push(backgroundLayer);
 
-        const gravity = 0.5;
-
+        const gravity = 30;
+        mario.pos.set(64, 180);
+        mario.vel.set(200, -600);
 
 
         const spriteLayer = createSpriteLayer(mario);
         comp.layers.push(spriteLayer);
 
-        function update() {
+        const deltaTime = 1/60;
+        let lastTime = 0;
+
+        function update(time) {
+           // deltaTime = (time - lastTime) / 1000;
+            
             comp.draw(context);
-            mario.update();
+            mario.update(deltaTime);
             mario.vel.y += gravity;
             requestAnimationFrame(update);
+
+            lastTime = time;
         }
 
-        update();
+        update(0);
     });
