cda833afb234fd14df3692bbb053c7930c47230b
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-27 11:00:27 +0000

    Trait class and Velocity Trait created and implemented

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,8 @@ import { createMario } from './entities.js';
 import { loadBackgroundSprites } from './sprites.js'
 import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
+import Keyboard from './KeyboardState.js';
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
@@ -23,16 +25,27 @@ Promise.all([
         mario.pos.set(64, 180);
         mario.vel.set(200, -600);
 
+        const SPACE = 32;
+        const input = new Keyboard();
+        input.addMapping(SPACE, keyState => {
+            if (keyState) {
+                mario.jump.start();
+            } else {
+                mario.jump.cancel();
+            }
+            console.log(keyState);
+        });
+        input.listenTo(window);
 
         const spriteLayer = createSpriteLayer(mario);
         comp.layers.push(spriteLayer);
 
-        const timer = new Timer(1/60);
+        const timer = new Timer(1 / 60);
 
         timer.update = function update(deltaTime) {
-                mario.update(deltaTime);
-                comp.draw(context);
-                mario.vel.y += gravity * deltaTime;
+            mario.update(deltaTime);
+            comp.draw(context);
+            mario.vel.y += gravity * deltaTime;
         }
 
         timer.start();
