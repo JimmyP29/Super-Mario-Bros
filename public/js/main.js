3b53a0e55d20220109a49686a8eee23897dd6131
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-27 08:57:56 +0000

    Compositor class used for compositing sprite layers

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+import Compositor from './Compositor.js';
 import { loadLevel } from './loaders.js';
 import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
 
@@ -16,14 +17,56 @@ function drawBackground(background, context, sprites) {
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
+
+
+
+function createBackgroundLayer(backgrounds, sprites) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256;
+    buffer.height = 240;
+
+    backgrounds.forEach(background => {
+        drawBackground(background, buffer.getContext('2d'), sprites);
+    });
+
+    return function drawBackgroundLayer(context) {
+        context.drawImage(buffer, 0, 0);
+    };
+}
+
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, pos.x, pos.y);
+    }
+}
+
 Promise.all([
     loadMarioSprite(),
     loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
     .then(([marioSprite, sprites, level]) => {
-        level.backgrounds.forEach(background => {
-            drawBackground(background, context, sprites);
-        });
-        marioSprite.draw('idle', context, 64, 64);
+        const comp = new Compositor();
+
+        const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+        comp.layers.push(backgroundLayer);
+
+        const pos = {
+            x: 64,
+            y: 64
+        };
+
+        const spriteLayer = createSpriteLayer(marioSprite, pos);
+        comp.layers.push(spriteLayer);
+
+        function update() {
+            comp.draw(context);
+
+            pos.x += 2;
+            pos.y += 2;
+
+            requestAnimationFrame(update);
+        }
+
+        update();
     });
